1946b5c9b7456a3c1077142c9eae568420fbeecc
Simon Repp <simon@fdpl.io>
2018-05-27 14:41:35 +0200

    Support escaped section names, visually distinguish dictionary entries

--- prism-eno.js
+++ prism-eno.js
@@ -13,24 +13,41 @@ Prism.languages.eno = {
     },
     lookbehind: true
   },
+  'escaped-section-3rd-plus': {
+    pattern: /(^|\n)[^\S\n]*#{3,}(?!#)[^\S\n]*(`+)(?:(?!\2).)*\2/,
+    lookbehind: true
+  },
   'section-3rd-plus': {
     pattern: /(^|\n)[^\S\n]*#{3,}(?!#)[^\S\n]*[^\s<][^\n<]*/,
+    lookbehind: true
+  },
+  'escaped-section-2nd': {
+    pattern: /(^|\n)[^\S\n]*##(?!#)[^\S\n]*(`+)(?:(?!\2).)*\2/,
     lookbehind: true
   },
   'section-2nd': {
     pattern: /(^|\n)[^\S\n]*##(?!#)[^\S\n]*[^\s<][^\n<]*/,
     lookbehind: true
   },
+  'escaped-section-1st': {
+    pattern: /(^|\n)[^\S\n]*#(?!#)[^\S\n]*(`+)(?:(?!\2).)*\2/,
+    lookbehind: true
+  },
   'section-1st': {
     pattern: /(^|\n)[^\S\n]*#(?!#)[^\S\n]*[^\s<][^\n<]*/,
     lookbehind: true
   },
-  'escaped': {
-    pattern: /(^|\n)\s*(`+)(?:(?!\2).)*\2+/,
+  'escaped-name': {
+    pattern: /(^|\n)[^\S\n]*(`+)(?:(?!\2).)*\2[:<]/,
+    lookbehind: true,
     alias: 'name'
   },
   'name': {
-    pattern: /(^|\n)[^\S\n]*[^:=<\n\s>|\-\\#][^:=<\n]*/,
+    pattern: /(^|\n)[^\S\n]*[^:=<\n\s>|\-\\#][^:=<\n]*[:<]/,
+    lookbehind: true
+  },
+  'entry': {
+    pattern: /(^|\n)[^\S\n]*[^:=<\n\s>|\-\\#][^:=<\n]*[=]/,
     lookbehind: true
   },
   'append': {
